9a4053b312dc5de966af788828eddc0ef9387fef
Boas-Bas van der Veen <Boas.Bas@Gmail.com>
2023-03-13 21:46:12 +0100

    Added Update to GameObject

--- game.cpp
+++ game.cpp
@@ -1,29 +1,63 @@
 #include "game.h"
 
 namespace Tmpl8 {
-	GameObject::GameObject(Sprite* a_Sprite, vec2 a_Position, vec2 a_Velocity, bool a_IsDynamic) :
+	//GameObject class constructors
+	GameObject::GameObject(Sprite& a_Sprite, vec2 a_Position, vec2 a_Velocity, bool a_IsDynamic) :
 		m_Sprite(a_Sprite),
 		m_Position(a_Position),
 		m_Velocity(a_Velocity),
 		m_ObjectType((GameObject::ObjectType)a_IsDynamic) {
 	}
 
-	GameObject::GameObject(Sprite* a_Sprite, vec2 a_Position) :
+	GameObject::GameObject(Sprite& a_Sprite, vec2 a_Position) :
 		m_Sprite(a_Sprite),
 		m_Position(a_Position), 
 		m_ObjectType(ObjectType::Static) {
 	}
 
-	GameObject::~GameObject() {
-		delete m_Sprite;
+	//GameObject special operations
+	void GameObject::Update() {
+		// Update the position if this is a dynamic GameObject
+		if (m_ObjectType == ObjectType::Dynamic) {
+			// Ground check
+			groundHit = (m_Position.y >= ScreenHeight - m_Sprite.GetHeight() - 1) ? true : false;
+
+			// Apply gravity to the velocity 
+			if (!groundHit) {
+				m_Velocity.y += (float) gravity * dt;
+			}
+
+			// Update the velocity
+			m_Velocity *= (float) deceleration;
+
+			// Update position
+			m_Position += m_Velocity;
+		}
+
+		// Collision check
+
+		// Only draw the object if it is on the screen
+		if (m_Position.x < ScreenWidth && m_Position.x + m_Sprite.GetWidth() > 0 &&
+			m_Position.y < ScreenHeight && m_Position.y + m_Sprite.GetHeight() > 0) {
+			m_Sprite.Draw(screen, (int)m_Position.x, (int)m_Position.y);
+		}
 	}
 
+	////////////////////
+	//		GAME	  //
+	////////////////////
+
 	// Variables (Main Scene)
-	GameObject startButton(new Sprite(new Surface("assets/start_button.tga"), 2), vec2(300, 412));
+	Sprite s_StartButton(new Surface("assets/start_button.tga"), 2);
+	GameObject startButton(s_StartButton, vec2(300, 412));
 
 	// Variables (Game Scene)
-	GameObject player(new Sprite(new Surface("assets/ball.png"), 1), vec2(375, 462), vec2(4, -20), true);
-	GameObject platform(new Sprite(new Surface("assets/balk.png"), 1), vec2(500, 200));
+	Sprite s_Player(new Surface("assets/ball.png"), 1);
+	GameObject player(s_Player, vec2(375, 462), vec2(4, -20), true);
+	Sprite s_Platform(new Surface("assets/balk.png"), 1);
+	GameObject platform(s_Platform, vec2(500, 200));
+
+	GameObject sceneObjects[10];
 
 	constexpr float maxBoost = 2;
 	float boost = 0;
@@ -54,8 +88,7 @@ namespace Tmpl8 {
 				// Update velocity down and reset velocity when groundHit
 				if (input->GetKey(SDL_SCANCODE_SPACE) && !groundHit) {
 					player.GetVelocity().y = 100;
-				}
-				if (input->GetKey(SDL_SCANCODE_SPACE) && groundHit) {
+				} else if (input->GetKey(SDL_SCANCODE_SPACE) && groundHit) {
 					boost = (boost < maxBoost) ? (boost += dt) : maxBoost;
 					player.GetVelocity() = {0, 0};
 				}
@@ -68,73 +101,47 @@ namespace Tmpl8 {
 					boost = 0;
 				}
 
-				Physics(player, dt);
-				Collision(player);
-
-				player.GetSprite()->Draw(screen, (int) player.GetPosition().x, (int) player.GetPosition().y);
-				platform.GetSprite()->Draw(screen, (int)platform.GetPosition().x, (int)platform.GetPosition().y);
+				player.Update();
+				platform.Update();
 				break;
 			default:
 				break;
 		}
 	}
 
-	/*bool CheckCollision(const Circle& circle, const Rect& rect) {
-		float closestX = std::max(rect.x, std::min(circle.x, rect.x + rect.width));
-		float closestY = std::max(rect.y, std::min(circle.y, rect.y + rect.height));
-		float distance = sqrt(pow(circle.x - closestX, 2) + pow(circle.y - closestY, 2));
-		return distance <= circle.radius;
-	}*/
+	//void Game::Collision(GameObject& p) {
+	//	// Screen boundry collision
+	//	if (p.GetPosition().x < 0 || p.GetPosition().x + p.GetSprite().GetWidth() > ScreenWidth) {
+	//		p.GetPosition().x = std::max((float) 0, std::min(p.GetPosition().x, (float) ScreenWidth - p.GetSprite().GetWidth()));
+	//		p.GetVelocity().x = -p.GetVelocity().x;
+	//	}
+	//	if (p.GetPosition().y < 0 || p.GetPosition().y + p.GetSprite().GetHeight() > ScreenHeight) {
+	//		p.GetPosition().y = std::max((float) 0, std::min(p.GetPosition().y, (float) ScreenHeight - p.GetSprite().GetHeight()));
+	//		p.GetVelocity().y = -p.GetVelocity().y;
+	//	}
 
-	void Game::Physics(GameObject& p, float dt) {
-		// Ground check
-	 	groundHit = (p.GetPosition().y >= ScreenHeight - p.GetSprite()->GetHeight() - 1) ? true : false;
-
-		// Apply gravity to the velocity 
-		if (!groundHit) {
-			p.GetVelocity().y += (float)gravity * dt;
-		}
-
-		// Update the velocity
-		p.GetVelocity() *= (float)deceleration;
-
-		// Update position
-		p.GetPosition() += p.GetVelocity();
-	}
-
-	void Game::Collision(GameObject& p) {
-		// Screen boundry collision
-		if (p.GetPosition().x < 0 || p.GetPosition().x + p.GetSprite()->GetWidth() > ScreenWidth) {
-			p.GetPosition().x = std::max((float) 0, std::min(p.GetPosition().x, (float) ScreenWidth - p.GetSprite()->GetWidth()));
-			p.GetVelocity().x = -p.GetVelocity().x;
-		}
-		if (p.GetPosition().y < 0 || p.GetPosition().y + p.GetSprite()->GetHeight() > ScreenHeight) {
-			p.GetPosition().y = std::max((float) 0, std::min(p.GetPosition().y, (float) ScreenHeight - p.GetSprite()->GetHeight()));
-			p.GetVelocity().y = -p.GetVelocity().y;
-		}
-
-		// Object collision
-		// TODO: Array of all object on screen
-		/*for each (object var in collection_to_loop) {
-		}*/
-		if (p.GetPosition().x <= platform.GetPosition().x + platform.GetSprite()->GetWidth() &&
-			p.GetPosition().x + p.GetSprite()->GetWidth() >= platform.GetPosition().x &&
-			p.GetPosition().y <= platform.GetPosition().y + platform.GetSprite()->GetHeight() &&
-			p.GetPosition().y + p.GetSprite()->GetHeight() >= platform.GetPosition().y) {
-			
-			p.GetVelocity().x = -p.GetVelocity().x;
-			p.GetVelocity().y = -p.GetVelocity().y;
-		}
-	}
+	//	// Object collision
+	//	// TODO: Array of all object on screen
+	//	/*for each (object var in collection_to_loop) {
+	//	}*/
+	//	if (p.GetPosition().x <= platform.GetPosition().x + platform.GetSprite().GetWidth() &&
+	//		p.GetPosition().x + p.GetSprite().GetWidth() >= platform.GetPosition().x &&
+	//		p.GetPosition().y <= platform.GetPosition().y + platform.GetSprite().GetHeight() &&
+	//		p.GetPosition().y + p.GetSprite().GetHeight() >= platform.GetPosition().y) {
+	//		
+	//		p.GetVelocity().x = -p.GetVelocity().x;
+	//		p.GetVelocity().y = -p.GetVelocity().y;
+	//	}
+	//}
 
 	bool Game::Button(GameObject sObj) {
-		sObj.GetSprite()->SetFrame(0);
-		sObj.GetSprite()->Draw(screen, (int)sObj.GetPosition().x, (int) sObj.GetPosition().y);
-		if (input->GetMousePos().x >= (int) sObj.GetPosition().x && input->GetMousePos().x <= (int) sObj.GetPosition().x + sObj.GetSprite()->GetWidth()) {
-			if (input->GetMousePos().y >= (int) sObj.GetPosition().y && input->GetMousePos().y <= (int) sObj.GetPosition().y + sObj.GetSprite()->GetHeight()) {
+		sObj.GetSprite().SetFrame(0);
+		sObj.GetSprite().Draw(screen, (int)sObj.GetPosition().x, (int) sObj.GetPosition().y);
+		if (input->GetMousePos().x >= (int) sObj.GetPosition().x && input->GetMousePos().x <= (int) sObj.GetPosition().x + sObj.GetSprite().GetWidth()) {
+			if (input->GetMousePos().y >= (int) sObj.GetPosition().y && input->GetMousePos().y <= (int) sObj.GetPosition().y + sObj.GetSprite().GetHeight()) {
 				if (input->GetMouseButton(1)) {
-					sObj.GetSprite()->SetFrame(1);
-					sObj.GetSprite()->Draw(screen, (int) sObj.GetPosition().x, (int) sObj.GetPosition().y);
+					sObj.GetSprite().SetFrame(1);
+					sObj.GetSprite().Draw(screen, (int) sObj.GetPosition().x, (int) sObj.GetPosition().y);
 				}
 				if (input->GetMouseButtonUp(1)) {
 					return true;
--- game.h
+++ game.h
@@ -12,22 +12,21 @@ namespace Tmpl8 {
 	class Input;
 
 	class GameObject {
-		enum class ObjectType { Static, Dynamic };
+		enum class ObjectType { Static, Dynamic }; // I made it a enum so I could add more types later... maybe
 	public:
 		// constructor / destructor
-		GameObject(Sprite* a_Sprite, vec2 a_Position, vec2 a_Velocity, bool a_IsDynamic = false);
-		GameObject(Sprite* a_Sprite, vec2 a_Position);
-		~GameObject();
+		GameObject(Sprite& a_Sprite, vec2 a_Position, vec2 a_Velocity, bool a_IsDynamic = false);
+		GameObject(Sprite& a_Sprite, vec2 a_Position);
+		~GameObject() = default;
 		// member data access
-		Sprite* GetSprite() { return m_Sprite; }
+		Sprite& GetSprite() { return m_Sprite; }
 		vec2& GetPosition() { return m_Position; }
-		void SetPosition(vec2 a_Position) { m_Position = a_Position; }
 		vec2& GetVelocity() { return m_Velocity; }
-		void SetVelocity(vec2 a_Velocity) { m_Velocity = a_Velocity; }
 		// special operations
+		void Update() {}
 	private:
 		// attributes
-		Sprite* m_Sprite;
+		Sprite& m_Sprite;
 		vec2 m_Position;
 		vec2 m_Velocity;
 		ObjectType m_ObjectType;
@@ -50,8 +49,6 @@ namespace Tmpl8 {
 		const double deceleration = 0.995;
 
 		bool Button(GameObject sObj);
-		void Physics(GameObject& p, float dt);
-		void Collision(GameObject& p);
 		void ChangeScene(Scene newScene) { curScene = newScene; screen->Clear(0); };
 	};
 };
